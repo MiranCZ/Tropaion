0bb138924a9b3782c1146f3179eeaf562ef28a5e
MiranCZ <miraancz@gmail.com>
2026-03-23 10:06:44 +0100

    Feat: Convert tuples

--- src/util/arg_convertor.rs
+++ src/util/arg_convertor.rs
@@ -1,13 +1,15 @@
+use crate::analysis::type_registry::TypeRegistry;
 use crate::ast::expression::int;
 use crate::interpreter::interpreter::Interpreter;
 use crate::interpreter::value::Value;
 use crate::interpreter::value::Value::{FloatValue, IntValue, RefValue};
-use crate::util::arg_convertor::ValueConvertable::{FloatValueConv, IntValueConv, VecValueConv};
+use crate::util::arg_convertor::ValueConvertable::{FloatValueConv, IntValueConv, TupleValueConv, VecValueConv};
 
 pub enum ValueConvertable {
     IntValueConv(i32),
     FloatValueConv(f32),
-    VecValueConv(Vec<ValueConvertable>)
+    VecValueConv(Vec<ValueConvertable>),
+    TupleValueConv(Vec<ValueConvertable>)
 }
 
 impl ValueConvertable {
@@ -17,6 +19,14 @@ impl ValueConvertable {
             IntValueConv(_) => "i".to_string(),
             FloatValueConv(_) => "f".to_string(),
             VecValueConv(_) => "LVec;".to_string(),
+            TupleValueConv(types) => {
+                let mut name = "T".to_string();
+                for t in types {
+                    name += t.get_mangled().as_str();
+                }
+
+                name + ";"
+            }
         }
     }
 
@@ -24,6 +34,24 @@ impl ValueConvertable {
         match self {
             IntValueConv(i) => vec![IntValue(i)],
             FloatValueConv(f) => vec![FloatValue(f)],
+            TupleValueConv(types) => {
+                let ptr = interpreter.stack_top();
+
+                let rf = RefValue {ptr, len: types.len() as u32};
+
+                let mut result = vec![];
+                for x in types {
+                    let value = x.into_value(interpreter);
+
+                    for v in value {
+                        result.push(v);
+                    }
+                }
+
+                result.push(rf);
+
+                result
+            }
             VecValueConv(values) => {
                 // vector is ptr -> (capacity, len, arr_ptr)
                 let ptr = interpreter.stack_top();
@@ -92,3 +120,46 @@ impl <T: ValueLike> ValueLike for Vec<T> {
 pub fn into_arg<T: ValueLike>(value: T) -> ValueConvertable {
     value.into_convertable()
 }
+
+
+// tuple impl... just- no
+macro_rules! tuple_impls {
+    ( $head:ident, $( $tail:ident, )+ ) => {
+        impl<$head, $( $tail ),+> ValueLike for ($head, $( $tail ),+)
+        where
+            $head: ValueLike,
+            $( $tail: ValueLike ),+
+        {
+
+            fn into_convertable(self) -> ValueConvertable {
+                #[allow(non_snake_case)]
+                let ($head, $( $tail ),+) = self;
+
+                let values = vec![$head.into_convertable(), $( $tail.into_convertable() ),+];
+
+                TupleValueConv(values)
+            }
+        }
+
+        tuple_impls!($( $tail, )+);
+    };
+
+    ( $head:ident, ) => {
+        impl<$head> ValueLike for ($head,)
+        where
+            $head: ValueLike,
+        {
+            fn into_convertable(self) -> ValueConvertable {
+                #[allow(non_snake_case)]
+                let ($head,) = self;
+
+                let values = vec![$head.into_convertable()];
+                TupleValueConv(values)
+            }
+        }
+    };
+
+    () => {};
+}
+
+tuple_impls!(A, B, C, D, E, F, G, H, I, J,);
